bc782fbff83b7399b44745123edda37ed3dbeb71
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-03-11 04:27:53 -0300

    detalles

--- src/views/Item.tsx
+++ src/views/Item.tsx
@@ -18,7 +18,7 @@ const Item = () => {
 	}
 
 	const handdlePagarMP = () => {
-		const PORT = 8080;
+		// const PORT = 8080;
 		// const APIURL = `http:///localhost:${PORT}/api/`;
 		const APIURL = `https://checkout-mp-react.herokuapp.com/`;
 		const PUBLIC_KEY = 'TEST-efcbcded-34c5-494c-9a53-fbaa41af4f41';
