1feadfbe5900c5b1891244820e9fc600c83f4676
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-03-11 05:54:01 -0300

    se quita iframe que genera mercadolibre automaticamentre.

--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 const useScript = (url = '', props = {}) => {
 	useEffect(() => {
 		const script = document.createElement('script');
-
 		script.src = url;
 		script.async = true;
 		Object.keys(props).forEach((key) => {
@@ -11,6 +10,11 @@ const useScript = (url = '', props = {}) => {
 		});
 		document.body.appendChild(script);
 		return () => {
+			//esto es para limpiar el iframe que genera el script de mercado libre.
+			let iframe = document.body.querySelector('iframe[src*="mercadolibre"]');
+			if (iframe) {
+				document.body.removeChild(iframe);
+			}
 			document.body.removeChild(script);
 		};
 	}, [url]);
